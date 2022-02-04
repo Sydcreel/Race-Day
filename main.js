209fe76d01ef0a494d20b7f1b994727de147667d
Sydney Creel <sydney.creel@outlook.com>
2022-02-03 20:15:40 -0800

    add functions to determine runner age, registration status, and race start time

---
+++ main.js
@@ -0,0 +1,25 @@
+// race numbers are assigned randomly.
+let raceNumber = Math.floor(Math.random() * 1000);
+
+// indicates whether a runner registered early or not.
+let earlyRegister = true;
+
+// runner's age.
+let runnerAge = 12;
+
+// checks whether runner is adult AND registered early.
+if (earlyRegister && runnerAge > 18){
+    raceNumber += 1000;
+}
+
+// checks age and registration time to determine race time.
+if (earlyRegister && runnerAge > 18){
+    console.log(`Hello, #${raceNumber}! Your race begins at 9:30 A.M.`);
+} else if (earlyRegister = false && runnerAge > 18){
+    console.log(`Hello, #${raceNumber}! Your race begins at 11:00 A.M.`);
+}
+
+// tells runners under 18 that race will start at 12:30pm.
+if (runnerAge < 18){
+    console.log(`Hello, #${raceNumber}! Your race begins at 12:30 P.M.`);
+}
